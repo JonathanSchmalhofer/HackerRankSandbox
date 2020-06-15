eeabf5ca1ea388518ae41413ec5382c15eb682e3
JonathanSchmalhofer <jonathan.schmalhofer@gmail.com>
2020-06-15 18:37:36 +0200

    add Kangaroo

---
+++ problem_solving/16_kangaroo/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the kangaroo function below.
+string kangaroo(int x1, int v1, int x2, int v2) {
+    double numerator = static_cast<double>(x2 - x1);
+    double denominator = static_cast<double>(v1 - v2);
+    std::cout << "num = " << numerator << ", den = " << denominator << ", fmod(num/den) = " << std::fmod(numerator, denominator) << std::endl;
+    if (std::fmod(numerator, denominator) == 0 && (numerator/denominator) > 0) {
+        return std::string("YES");
+    }
+    else {
+        return std::string("NO");
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string x1V1X2V2_temp;
+    getline(cin, x1V1X2V2_temp);
+
+    vector<string> x1V1X2V2 = split_string(x1V1X2V2_temp);
+
+    int x1 = stoi(x1V1X2V2[0]);
+
+    int v1 = stoi(x1V1X2V2[1]);
+
+    int x2 = stoi(x1V1X2V2[2]);
+
+    int v2 = stoi(x1V1X2V2[3]);
+
+    string result = kangaroo(x1, v1, x2, v2);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
